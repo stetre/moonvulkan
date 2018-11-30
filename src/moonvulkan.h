c667272a905c5029ef3f9b8cbfd166c60796bbb1
Stefano Trettel <s_trettel@yahoo.com>
2018-11-30 10:53:19 +0100

    Version 0.8 (Vulkan 1.1.92)

--- src/moonvulkan.h
+++ src/moonvulkan.h
@@ -38,7 +38,7 @@
 #define VK_NO_PROTOTYPES 1
 #include "vulkan/vulkan.h"  /* include locally from moonvulkan/src/vulkan/ */
 
-#define MOONVULKAN_VERSION      "0.7"
+#define MOONVULKAN_VERSION      "0.8"
 
 #endif /* moonvulkanDEFINED */
 
