00e81c9233fd22b8e5478b3c2df2a9acbb7210be
Stefano Trettel <s_trettel@yahoo.com>
2021-11-29 20:08:41 +0100

    Version 0.9

--- src/moonvulkan.h
+++ src/moonvulkan.h
@@ -39,7 +39,7 @@
 #define VK_NO_PROTOTYPES 1
 #include "vulkan/vulkan.h"  /* include locally from moonvulkan/src/vulkan/ */
 
-#define MOONVULKAN_VERSION      "0.8"
+#define MOONVULKAN_VERSION      "0.9"
 
 #endif /* moonvulkanDEFINED */
 
